7dcfd61e4176d4061314e81b51fecdcc021833a9
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-28 14:11:35 -0300

    defiindo style apenas para o escopo do componente

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -14,7 +14,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* estilo do painel */
 
 .painel {
@@ -36,5 +36,9 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+}
+
+* {
+    box-shadow: 5px 5px 5px black;
 }
 </style>
