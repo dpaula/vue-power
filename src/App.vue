1f68786e3d96655733109065b91d94caba2a928f
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-28 13:22:47 -0300

    definindo painel para imagens

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <!-- // Este é um SFT (Single File Template) que é um arquivo vue que define 3 partes: template, script e style -->
 
+
 <template>
   <div class="corpo">
     <!-- usando interpolação para acesso dos dados, neste caso o titulo -->
@@ -7,10 +8,18 @@
       <h1>{{ titulo }}</h1>
     </div>
     <!-- usando v-bind para acesso direto a um objeto dentro dos dados -->
-    <div >
+    <div>
       <ul class="lista-fotos">
         <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
-          <img :src="foto.url" :alt="foto.titulo">
+          
+          <div class="painel">
+            <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+            <div class="painel-conteudo">
+
+                <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+            </div>
+          </div>
+
         </li>
       </ul>
     </div>
@@ -39,23 +48,48 @@ export default {
 </script>
 
 <style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
 
-    .corpo {
-        font-family: Helvetica, sans-serif;
-        width: 96%;
-        margin: 0 auto;
-    }
+.titulo {
+  text-align: center;
+}
 
-    .titulo {
-        text-align: center;
-    }
+.lista-fotos {
+  list-style: none;
+}
 
-    .lista-fotos {
-        list-style: none;
-    }
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 
-    .lista-fotos .lista-fotos-item {
-        display: inline-block;
-    }
+.imagem-responsiva {
+    width: 100%;
+}
 
+/* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
