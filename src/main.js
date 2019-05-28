2290be525e946f690728354ab11b711d89f9d83b
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-28 11:09:01 -0300

    instalando vue-resource e registrando na aplicação

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// importando a dependência vue-resource que já foi instalada e localizada no node_modules
+import VueResource from 'vue-resource';
+
+// registrando o uso da dependência
+Vue.use(VueResource);
+
 new Vue({
   el: '#app',
   render: h => h(App)
