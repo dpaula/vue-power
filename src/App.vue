e2e2fd3a2740457fd1fd021f5cd371c7adc84632
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-28 10:53:10 -0300

    usando diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
+<!-- // Este é um SFT (Single File Template) que é um arquivo vue que define 3 partes: template, script e style -->
+
 <template>
-  <div>
+<div>
     <!-- usando interpolação para acesso dos dados, neste caso o titulo -->
     <h1>{{ titulo }}</h1>
-      <!-- usando v-bind para acesso direto a um objeto dentro dos dados -->
-    <img
-      :src="foto.url"
-      :alt="foto.titulo"
-    >
-  </div>
+    <!-- usando v-bind para acesso direto a um objeto dentro dos dados -->
+
+    <ul>
+        <li v-for="foto of fotos" :key="foto.id">
+            <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      titulo: 'App Power',
-      foto: {
-        url: 'http://www.sebrae.com.br/Sebrae/Portal%20Sebrae/UFs/AP/Imagens/tecnologia.png',
-        titulo: 'Tecnologia'
-      }
+    data() {
+        return {
+            titulo: "App Power",
+            fotos: [{
+                    id: 1,
+                    url: "http://www.sebrae.com.br/Sebrae/Portal%20Sebrae/UFs/AP/Imagens/tecnologia.png",
+                    titulo: "Tecnologia"
+                },
+                {
+                    id: 2,
+                    url: "http://grupoatta.com.br/site/wp-content/uploads/2016/09/politico.jpg",
+                    titulo: "Politica"
+                }
+            ]
+        };
     }
-  },
-
 };
 </script>
 
