b264c98497333b75cd66ccef1fda9cb88f747bbb
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-29 13:56:50 -0300

    passando parametros para directive

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -7,8 +7,11 @@ Vue.directive('meu-transform', {
 
     //metodo principal da diretiva
     bind(el, binding, vnode) {
-        
-        rotacionar(el);
+
+        //propriedades passadas para a diretiva
+        //v-meutransform="{ incremento: 20, animate: true }"
+
+        rotacionar(el, binding);
 
     }
 
@@ -19,11 +22,28 @@ Vue.directive('meu-transform', {
  * 
  * @param {dom} el 
  */
-function rotacionar(el) {
+function rotacionar(el, binding) {
+
+
     let current = 0;
+
     el.addEventListener('dblclick', function () {
-        current += 90;
+
+        // se retornar nada, atribui 90;
+        let incremento = binding.value.incremento || 90;
+
+        current += incremento;
+
+        // rotaciona em graus
         el.style.transform = `rotate(${current}deg)`;
+
+        if(binding.value.animate){
+            // a trasição da rotação leva meio segundo
+            el.style.transition = 'transform 0.5s'
+        }
+
     });
+
 }
+
 
