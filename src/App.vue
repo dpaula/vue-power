061232dcc66bb481f1657d598b1af7aef3b43e44
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-29 09:48:52 -0300

    cnofigurando menu para link das rotas

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <!-- // Este é um SFT (Single File Template) que é um arquivo vue que define 3 partes: template, script e style -->
 
 <template>
-<div class="corpo">
+  <div class="corpo">
+    <nav>
+      <ul>
+        <li v-for="route in routes" :key="route.id">
+          <router-link :to="route.path ? route.path : '/'">{{ route.titulo}}</router-link>
+        </li>
+      </ul>
+    </nav>
+
     <router-view></router-view>
-</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { routes } from "./routes";
+
+export default {
+  data() {
+    return {
+      routes
+    };
+  }
+};
 </script>
 
 <style>
 .corpo {
-    font-family: Helvetica, sans-serif;
-    width: 96%;
-    margin: 0 auto;
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
 }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Home from './components/home/Home.vue'
 export const routes = [
 
     //rota para localhost:8080/#/ apontando para componente Home
-    { path: '', component: Home },
+    { path: '', component: Home, titulo: 'Home', id: 1 },
     //rota para localhost:8080/#/cadastro apontando para componente Home
-    { path: '/cadastro', component: Cadastro }
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro', id: 2 }
 ];
